f7aa5686711b85db03e796eb4d79a8a28d2711fe
Musis Ferreira <feativa@gmail.com>
2025-07-25 15:40:58 -0300

    commit error new

--- src/gemini/use-cases/chat-prompt-stream.use-case.ts
+++ src/gemini/use-cases/chat-prompt-stream.use-case.ts
@@ -3,6 +3,8 @@ import { ChatPromptDto } from '../dtos/chat-prompt.dto';
 import { geminiUploadFiles } from '../helpers/gemini-upload-file';
 
 
+
+
 interface Options{
     model?: string;
     systemInstruction?: string;
@@ -14,7 +16,9 @@ export const chatPromptStreamUseCase = async (
     options?: Options,
     ) => {
         const { prompt, files = [] } = chatPromptDto;
-        const images = await geminiUploadFiles(ai, files);
+        const uploadedFiles = await geminiUploadFiles(ai, files);
+
+
 
         const {
             model = 'gemini-2.0-flash',
